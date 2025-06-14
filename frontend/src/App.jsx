ee7d23f9e6f5f17eb33f8218c4dff69a2010ac5a
Muhammad Tayyab Nawaz <m.tayyab33600@gmail.com>
2025-06-14 15:05:24 +0500

    add basic routes struucture

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p className='text-3xl text-center'>EquiLife</p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
